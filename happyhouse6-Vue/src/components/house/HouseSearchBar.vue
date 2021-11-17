1ea12d8b11ff3abaad5bed172ad513b0aa60ee35
송민주 <dbfm2222@naver.com>
2021-11-17 09:40:52 +0900

    뷰 프로젝트 first init

---
+++ happyhouse6-Vue/src/components/house/HouseSearchBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-row class="mt-4 mb-4 text-center">
+    <!-- <b-col class="sm-3">
+      <b-form-input
+        v-model.trim="dongCode"
+        placeholder="동코드 입력...(예 : 11110)"
+        @keypress.enter="sendKeyword"
+      ></b-form-input>
+    </b-col>
+    <b-col class="sm-3" align="left">
+      <b-button variant="outline-primary" @click="sendKeyword">검색</b-button>
+    </b-col> -->
+    <b-col class="sm-3">
+      <b-form-select
+        v-model="sidoCode"
+        :options="sidos"
+        @change="gugunList"
+      ></b-form-select>
+    </b-col>
+    <b-col class="sm-3">
+      <b-form-select
+        v-model="gugunCode"
+        :options="guguns"
+        @change="searchApt"
+      ></b-form-select>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { mapState, mapActions, mapMutations } from "vuex";
+
+export default {
+  name: "HouseSearchBar",
+  data() {
+    return {
+      sidoCode: null,
+      gugunCode: null,
+    };
+  },
+  computed: {
+    ...mapState(["sidos", "guguns", "houses"]),
+    // sidos() {
+    //   return this.$store.state.sidos;
+    // },
+  },
+  created() {
+    // ! store에 있는 actions를 호출
+    this.sidoList();
+    this.gugunList();
+    // this.$store.dispatch("getSido");
+  },
+  methods: {
+    ...mapActions(["getSido", "getGugun", "getHouseList"]),
+    ...mapMutations(["CLEAR_SIDO_LIST", "CLEAR_GUGUN_LIST"]),
+    sendKeyword() {},
+    sidoList() {
+      this.CLEAR_SIDO_LIST();
+      this.getSido();
+    },
+    gugunList() {
+      console.log(this.sidoCode);
+      this.CLEAR_GUGUN_LIST();
+      if (this.sidoCode) {
+        this.getGugun(this.sidoCode);
+      }
+    },
+    searchApt() {
+      if (this.gugunCode) {
+        this.getHouseList(this.gugunCode);
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
