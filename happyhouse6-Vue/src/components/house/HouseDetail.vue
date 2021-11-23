2fdaff6b66c9aec5a06581ba6ca7ceaa13973a30
송민주 <dbfm2222@naver.com>
2021-11-23 16:13:10 +0900

    recover2

--- happyhouse6-Vue/src/components/house/HouseDetail.vue
+++ happyhouse6-Vue/src/components/house/HouseDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container v-if="house" class="bv-example-row">
     <b-row>
-      <b-col
+      <b-col class="back-white"
         ><h3>{{ house.아파트 }}</h3></b-col
       >
     </b-row>
@@ -18,8 +18,10 @@
   </b-container>
 </template>
 
+<!--<script type="text/javascript" src="../../api/map.js"></script>-->
 <script>
 import { mapState } from "vuex";
+// import { displayMarkers } from "@/api/map.js";
 
 const houseStore = "houseStore";
 
@@ -29,7 +31,13 @@ export default {
     return {};
   },
   computed: {
-    ...mapState(houseStore, ["house"]),
+    ...mapState(houseStore, [
+      "house",
+      "sidos",
+      "guguns",
+      "selectedSido",
+      "houses",
+    ]),
     // house() {
     //   return this.$store.state.house;
     // },
@@ -40,16 +48,89 @@ export default {
       return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
   },
-  mounted() {
+  created() {
     if (window.kakao && window.kakao.maps) {
       this.initMap();
     } else {
       const script = document.createElement("script");
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=47642a55e575541b111222ca754737af";
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=47642a55e575541b111222ca754737af&libraries=services";
       document.head.appendChild(script);
     }
+  },
+  watch: {
+    house: function (house) {
+      //console.log(house);
+      var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+        mapOption = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        };
+      // 지도를 생성합니다
+      var map = new kakao.maps.Map(mapContainer, mapOption);
+      // 주소-좌표 변환 객체를 생성합니다
+      var geocoder = new kakao.maps.services.Geocoder();
+      // 주소로 좌표를 검색합니다
+      var address = this.selectedSido.concat(house["법정동"], house["지번"]);
+      geocoder.addressSearch(address, function (result, status) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === kakao.maps.services.Status.OK) {
+          var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords,
+          });
+
+          var content = `
+				<div class="overlaybox">
+					<div class="boxtitle">${house.아파트}</div>
+					<div class="first"><img src="https://source.unsplash.com/MP0bgaS_d1c/600x300" style="width:247px; height:136px;" alt=""></div>
+					<ul>
+						<li class="up">
+							<span class="title">건축년도</span>
+							<span class="count">${house.건축년도}</span>
+						</li>
+						<li>
+							<span class="title">주소</span>
+							<span class="count">${address}</span>
+						</li>
+						<li>
+							<span class="title">최신거래금액</span>
+							<span class="count">${(parseInt(house.거래금액.replace(",", "")) * 10000)
+                .toString()
+                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</span>
+						</li>
+						<li>
+							<span class="last" id="recenthistor" data-toggle="modal" data-target="#myModal">아파트정보 update</span>
+						</li>
+					</ul>
+				</div>
+			`;
+          var position = new kakao.maps.LatLng(
+            marker.getPosition().getLat() + 0.00033,
+            marker.getPosition().getLng() - 0.00003
+          );
+
+          var customOverlay = new kakao.maps.CustomOverlay({
+            position: position,
+            content: content,
+            xAnchor: 0.3,
+            yAnchor: 0.91,
+          });
+          customOverlay.setMap(map);
+
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+          coords = new kakao.maps.LatLng(
+            marker.getPosition().getLat() + 0.0015,
+            marker.getPosition().getLng()
+          );
+          map.setCenter(coords);
+        }
+      });
+    },
+    // houses: function (houses) {},
   },
   methods: {
     initMap() {
@@ -73,4 +154,121 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.back-white {
+  filter: invert(100%);
+  font-weight: bold;
+}
+
+.overlaybox {
+  position: relative;
+  width: 360px;
+  height: 350px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/box_movie.png")
+    no-repeat;
+  padding: 15px 10px;
+}
+div.overlaybox div,
+ul {
+  margin: 0;
+  padding: 0;
+}
+.overlaybox li {
+  list-style: none;
+}
+.overlaybox .boxtitle {
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: left;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png")
+    no-repeat right 120px center;
+  margin-bottom: 8px;
+}
+.overlaybox .first {
+  position: relative;
+  width: 247px;
+  height: 136px;
+  margin-bottom: 8px;
+}
+.first .text {
+  color: #fff;
+  font-weight: bold;
+}
+.first .triangle {
+  position: absolute;
+  width: 48px;
+  height: 48px;
+  top: 0;
+  left: 0;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/triangle.png")
+    no-repeat;
+  padding: 6px;
+  font-size: 18px;
+}
+.first .movietitle {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.4);
+  padding: 7px 15px;
+  font-size: 14px;
+}
+.overlaybox ul {
+  width: 247px;
+}
+.overlaybox li {
+  position: relative;
+  margin-bottom: 2px;
+  background: #2b2d36;
+  padding: 5px 10px;
+  color: #aaabaf;
+  line-height: 1;
+  text-align: left;
+}
+.overlaybox li span {
+  display: inline-block;
+}
+.overlaybox li .number {
+  font-size: 16px;
+  font-weight: bold;
+}
+.overlaybox li .title {
+  font-size: 13px;
+}
+.overlaybox li .last {
+  font-size: 12px;
+  margin-right: 15px;
+}
+.overlaybox ul .arrow {
+  position: absolute;
+  margin-top: 8px;
+  right: 25px;
+  width: 5px;
+  height: 3px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/updown.png")
+    no-repeat;
+}
+.overlaybox >>> li >>> .up {
+  background-position: 0 -40px;
+}
+.overlaybox >>> li >>> .down {
+  background-position: 0 -60px;
+}
+.overlaybox li .count {
+  position: absolute;
+  margin-top: 5px;
+  right: 15px;
+  font-size: 10px;
+}
+.overlaybox li:hover {
+  color: #fff;
+  background: #d24545;
+}
+.overlaybox li:hover .up {
+  background-position: 0 0px;
+}
+.overlaybox li:hover .down {
+  background-position: 0 -20px;
+}
+</style>
